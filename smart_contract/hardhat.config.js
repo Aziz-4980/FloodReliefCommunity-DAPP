2a6c1542ad95ff54410bf1d4ebd1444f471901df
Abdul Aziz Khan <abdul.aziz.khan.1122@gmail.com>
2022-08-28 20:13:26 +0500

    block chian deployed and integrated to front end

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -4,7 +4,7 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     goerli: {
-      url: 'https://eth-goerli.g.alchemy.com/v2/rtNQHmtmrLrs7dIwb7NgoGyuuO7q1aWT',
+      url: 'https://eth-goerli.g.alchemy.com/v2/Yb4EycrM8MvlhjZIYtZu2IccyJuPRdb6',
       accounts: ['2743a850968330eed6fcdea7e239d421ed664e3bd9fb47008e75e36468d605f4'],
     },
   },
